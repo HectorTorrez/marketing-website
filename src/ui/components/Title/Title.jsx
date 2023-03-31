96546fc3aebd508084a90e05c5ab9450c166fbab
Hector <torrezhectorb@gmail.com>
2023-03-30 19:27:26 -0600

    change title

--- src/ui/components/Title/Title.jsx
+++ src/ui/components/Title/Title.jsx
@@ -4,7 +4,7 @@ export const Title = () => {
   return (
     <section className="container">
       <hgroup className="app-title">
-        <h1>Marketing Website</h1>
+        <h1>Shopping Cart</h1>
       </hgroup>
     </section>
   )
